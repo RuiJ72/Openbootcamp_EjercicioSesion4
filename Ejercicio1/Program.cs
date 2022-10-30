956fc8190c15fbe6d281063eba3fde410425b31c
RuiJ72 <rlagos900@gmail.com>
2022-10-29 23:18:36 -0300

    Ejercicio 1

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int numero, resultado;
+Console.WriteLine("Introduce el numero de la tabla de multiplicar que deseas: ");
+
+numero = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+
+for(int i = 1; i <= 10; i++)
+{
+    resultado = numero * i;
+    Console.WriteLine($"{numero} x {i} = {resultado}");
+}
+
+
+
